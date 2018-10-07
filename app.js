46ec4872b118c7cf63467410a46e896fed895d21
TwoFistedJustice <phaltak-1@yahoo.com>
2018-10-07 11:26:47 -0700

    Add POST/garage

--- app.js
+++ app.js
@@ -12,6 +12,27 @@ const {Bike} = require('./modelBike');
 
 app.use(bodyParser.json());
 
+
+//define a route to put a Garage in the db
+app.post('/garage', (req, res) => {
+  let garage = new Garage({name: req.body.name});
+  
+  garage.save()
+     .then((doc) => {
+       res.send(doc);
+     }, (e) => {
+      console.error('Unable to save garage');
+      res.status(400).send();
+     })
+  
+  
+});
+
+
+
+
+
+
 app.listen(3000, () => {
 console.log('Listening on port 3000');
 });
--- mongooseConfig.js
+++ mongooseConfig.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/CoffeeApp', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/NestedSchemas', { useNewUrlParser: true });
 module.exports = {mongoose};
